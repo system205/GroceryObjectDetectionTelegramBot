1847f8ba7eaf44e6008b94860b673ae908a3e7f9
system205 <arsenmutalapov205@gmail.com>
2023-11-03 11:37:08 +0300

    add confidence and iou properties for inference request in environment

--- src/main/java/com/inno/grocerybot/dto/InferRequest.java
+++ src/main/java/com/inno/grocerybot/dto/InferRequest.java
@@ -17,15 +17,17 @@ public class InferRequest {
     @JsonProperty("api_key")
     private String apiKey;
 
-    private float confidence = 0.7f;
+    private float confidence;
     @JsonProperty("iou_threshold")
-    private float iouThreshold = 0.5f;
+    private float iouThreshold;
 
     public InferRequest(String imageType, String imageValue, Environment env) {
         image.add(new Image(imageType, imageValue));
         this.modelId = env.getProperty("infer.request.model-id");
         this.modelType = env.getRequiredProperty("infer.request.model-type");
         this.apiKey = env.getProperty("infer.request.api-key");
+        this.confidence = Float.parseFloat(env.getProperty("infer.request.confidence", "0.7"));
+        this.iouThreshold = Float.parseFloat(env.getProperty("infer.request.iou-threshold", "0.5"));
     }
 
 
